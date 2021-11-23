478bd292c6769bec187f8e4d5a235596e94d377e
Erickinhou <willianerick17@gmail.com>
2021-11-23 13:48:00 -0300

    Add map on education part

--- src/components/MyResume.tsx
+++ src/components/MyResume.tsx
@@ -1,29 +1,69 @@
 export default function MyResume() {
+  const Educations = [
+    {
+      title:
+        " Bachelor of Computer Science (BCompSc) - Computer Engineering | CEFET-MG",
+      time: "August 2018 - July 2023 (Pretended)",
+      description: `With an emphasis in computer engineering learning about the low level processing conceits of a computer, I also have learned programming with bases in languages as Java, C, Assembly, Python and Haskell`,
+    },
+    {
+      title:
+        "Graduation Degree - Energy Engineering | Pontifical Catholic University PUC-MG",
+      time: "February 2018 - August 2018",
+      description: ` In the short period that I kept in this course, I was part of the
+      Baja PUC team; it's a competitive academic group in the off-road
+      automobile modality with many subareas os studying, as mechanical,
+      electrical and etc. It was the most important experience in this
+      period and you can check more in their`,
+      link: {
+        url: "https://sites.google.com/view/bajapucminas/página-inicial",
+        text: "site",
+      },
+    },
+    {
+      title: "Technician Degree - Electrical Engineering | CEFET-MG",
+      time: "February 2014 - December 2017",
+      description: `  I studied introduction and basics disciplines of electrical
+      education, and also have learned to program in MATLAB language. In
+      the period above 2016 to 2017, I was part of a robotic group that
+      has surged from mine and some friends idea, and we studied
+      residential automation and conceits of robotics utilizing Arduino
+      IDE with C language`,
+    },
+  ];
+
   return (
     <div className="my-resume">
       <div className="left">
         <h2>Education</h2>
-        <div className="education-item">
-          <h3>Bachelor of Computer Science (BCompSc) - Computer Engineering | CEFET-MG</h3>
-          <p>August 2018 - July 2023 (Pretended)</p>
-          <span>With an emphasis in computer engineering learning about the low level processing conceits of a computer, I also have learned programming with bases in languages as Java, C, Assembly, Python and Haskell.</span>
-          <div className="line"></div>
-        </div>
-        <div className="education-item">
-          <h3>Graduation Degree - Energy Engineering | Pontifical Catholic University PUC-MG</h3>
-          <p>February 2018 - August 2018</p>
-          <span>In the short period that I kept in this course, I was part of the Baja PUC team; it's a competitive academic group in the off-road automobile modality with many subareas os studying, as mechanical, electrical and etc. It was the most important experience in this period and you can check more in their <a href="https://sites.google.com/view/bajapucminas/página-inicial" target="#">site</a>.</span>
-          <div className="line"></div>
-        </div>
-        <div className="education-item">
-          <h3>Technician Degree - Electrical Engineering | CEFET-MG</h3>
-          <p>February 2014 - December 2017</p>
-          <span>I studied introduction and basics disciplines of electrical education, and also have learned to program in MATLAB language. In the period above 2016 to 2017, I was part of a robotic group that has surged from mine and some friends idea, and we studied residential automation and conceits of robotics utilizing Arduino IDE with C language.</span>
-          <div className="line"></div>
-        </div>
+        {Educations.map((education) => (
+          <div className="education-item">
+            <h3>{education.title}</h3>
+            <p>{education.time}</p>
+            <span>
+              {education.description}
+              {education.link && (
+                <a href={education.link.url} target="#">
+                  {education.link.text}
+                </a>
+              )}
+              .
+            </span>
+            <div className="line"></div>
+          </div>
+        ))}
         <h2>Curriculum</h2>
         <div className="my-curriculum">
-          <span>You can check my CV document in PDF with more details about me <a href="https://drive.google.com/file/d/1xfGA2qK1cKEhXdBXHRW__4QisQDe2LKm/view?usp=sharing" target="#">here</a>.</span>
+          <span>
+            You can check my CV document in PDF with more details about me{" "}
+            <a
+              href="https://drive.google.com/file/d/1xfGA2qK1cKEhXdBXHRW__4QisQDe2LKm/view?usp=sharing"
+              target="#"
+            >
+              here
+            </a>
+            .
+          </span>
         </div>
       </div>
 
@@ -32,27 +72,44 @@ export default function MyResume() {
         <div className="experience-item">
           <h3>Backend Developer | LeCode</h3>
           <p>September 2021 - Present</p>
-          <span>I work with JavaScript utilizing NodeJS to develop backend applications.</span>
+          <span>
+            I work with JavaScript utilizing NodeJS to develop backend
+            applications.
+          </span>
           <div className="line"></div>
         </div>
         <div className="experience-item">
           <h3>Marketing | Commit Jr.</h3>
           <p>March 2021 - Present</p>
-          <span>I am part of the Marketing team of Commit Jr. We manage the social medias of the company and work on projects to rise the networking, engagement and active prospection.</span>
+          <span>
+            I am part of the Marketing team of Commit Jr. We manage the social
+            medias of the company and work on projects to rise the networking,
+            engagement and active prospection.
+          </span>
           <div className="line"></div>
         </div>
         <div className="experience-item">
-          <h3>IT Support Analist | Federal Center of Technologial Education of Minas Gerais (CEFET-MG)</h3>
+          <h3>
+            IT Support Analist | Federal Center of Technologial Education of
+            Minas Gerais (CEFET-MG)
+          </h3>
           <p>November 2019 - October 2020</p>
-          <span>I worked with Java and Python languages, with addition of Git and databases conceits. I also learned a little about CSS, HTML and SQL.</span>
+          <span>
+            I worked with Java and Python languages, with addition of Git and
+            databases conceits. I also learned a little about CSS, HTML and SQL.
+          </span>
           <div className="line"></div>
         </div>
         <div className="experience-item">
           <h3>Client Support Analist | SKILL Informática</h3>
           <p>January 2016 - December 2016</p>
-          <span>I worked specifically with client support, realizing database backups remotely and problem solutions also remotely. I also worked with bug analysis and correction on the company's developed systems.</span>
+          <span>
+            I worked specifically with client support, realizing database
+            backups remotely and problem solutions also remotely. I also worked
+            with bug analysis and correction on the company's developed systems.
+          </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
