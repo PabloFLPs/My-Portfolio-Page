7a024a82271c75a9eb2a02ca0abcf2809654344a
Pablo Felipe <pablofelipe1999@hotmail.com>
2021-12-02 15:56:35 -0300

    Adding 'required' attribute to input fields

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,13 +3,13 @@ export default function Contact() {
     <div className="contact">
       <form id="contact-form" action="https://email-free-api.herokuapp.com/send-email" method="POST" target="#">
         <label>Name</label>
-        <input type="text" id="name" name="name" placeholder="Your Name"/>
+        <input type="text" id="name" name="name" placeholder="Your Name" required/>
 
         <label>Email</label>
-        <input type="email" id="email" name="email" placeholder="Your Email"/>
+        <input type="email" id="email" name="email" placeholder="Your Email" required/>
 
         <label>Message</label>
-        <textarea name="message" id="message" placeholder="Type a Message"></textarea>
+        <textarea name="message" id="message" placeholder="Type a Message" required/>
 
         <div className="send-message">
           <button type="submit">Send Message</button>
